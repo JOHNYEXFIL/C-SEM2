0c4969ad97d878d091525c2748a6159a1542b2b6
OLEG <oleg.bmx2004@gmail.com>
2024-02-27 18:04:27 +0300

    test 1

---
+++ TaskN1/TaskN1.cpp
@@ -0,0 +1,32 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+class CLASSITEM_N {
+private:
+	int I_NAME, I_INDEX, I_Amount, I_Date;
+	string I_Functions;
+public:
+	void massage() {
+		cout << "----------------------UNI ITEMS DATABASE----------------------"<<endl;
+	}
+
+	void set(int Name, int Index, int Amount, int Date, string Functions) {
+		I_NAME = Name;
+		I_INDEX = Index;
+		I_Amount = Amount;
+		I_Date = Date;
+		I_Functions = Functions;
+	}
+
+	void get() {
+		cout << "Name of ITEM:" << I_NAME << "INDEX OF I";
+	}
+};
+
+int main()
+{
+   
+}
+wdaadawdwawda
